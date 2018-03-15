e9aad5b5bc4b2b6c527df0a687f97366faf9794a
gael monbrault <gael.pro.dev@gmail.com>
2018-03-15 18:39:19 +0100

    ajout image et liaison storage firebase

--- src/components/FormBlog.vue
+++ src/components/FormBlog.vue
@@ -6,10 +6,10 @@
 
     <div class="container">
       <div class="row">
+
         <!-- Formulaire -->
         <div class="col-md-6">
           <form>
-            <!-- <input type="hidden" v-model="form.id" name="id"> -->
             <div class="form-group row">
               <label for="title">Titre de l'article</label>
               <input v-model="form.title" class="form-control" placeholder="titre de l'article" type="text" name="title" id="title">
@@ -18,13 +18,24 @@
               <label for="paragraphe">Paragraphe de l'article</label>
               <textarea v-model="form.paragraphe" class="form-control" placeholder="Description de l'article"  name="paragraphe" id="paragraphe" rows="4" cols="40"></textarea>
             </div>
+            <div class="form-group row">
+              <label for="image">Ajouter une image</label>
+              <input id="file" ref="fileInput" @change="handleFileSelected" class="form-control-file" type="file" accept="image/*">
+            </div>
             <button @click.prevent="formSave()" class="btn btn-primary" type="submit">Enregister</button>
           </form>
         </div>
+
         <!-- Rendu formulaire html -->
-        <div class="col-md-6">
-          <h1 v-model="form.title">{{ form.title }}</h1>
-        </div>
+        <!-- <div class="col-md-6">
+          <div class="card" style="width: 15rem;">
+            <img class="card-img-top" src="../assets/logo.png" alt="">
+            <div class="card-body">
+              <h5 class="card-title">{{ form.title }}</h5>
+              <p class="card-text">{{ form.paragraphe }}</p>
+            </div>
+          </div>
+        </div> -->
       </div>
     </div>
 
@@ -69,7 +80,7 @@
           </th>
           <th>
             <input type="text" v-model="value.contentArticle">
-            <button class="btn btn-primary" @click.prenvent="saveEdit(value)" type="button">
+            <button class="btn btn-primary" @click.prevent="saveEdit(value)" type="button">
               <i class="far fa-edit"></i>
             </button>
           </th>
@@ -97,6 +108,7 @@ let app = Firebase.initializeApp(config)
 let db = app.database()
 let BlogDb = db.ref('Blog/Article')
 let blogg = db.ref('Blog')
+let storage = app.storage()
 
 export default {
   name: 'FormBlog',
@@ -109,14 +121,30 @@ export default {
       form: [
         {
           title: '',
-          paragraphe: ''
+          paragraphe: '',
+          fileInput : ''
         }
       ]
     }
   },
   methods: {
+    // Vérification d'une image en upload
+    handleFileSelected (event) {
+      this.form.fileInput = event.target.files[0]
+      console.log(this.form.fileInput)
+    },
     // Sauvegarde d'un article et push() dans firebase
     formSave () {
+      let refImage = this.form.fileInput.name
+      let file = this.form.fileInput
+      let metadata = {
+        contentType: this.form.fileInput.type
+      }
+      console.log(this.form.fileInput.name)
+      //stockage des images dans firebase
+      const storageFile = storage.ref(refImage).put(file, metadata)
+      console.log(storageFile)
+      // //push des data text (titre et paragraphe)
       BlogDb.push(
         {
           titleArticle: this.form.title,
@@ -126,6 +154,7 @@ export default {
       )
       toastr.success('Article ajouté !')
     },
+    // Suppression d'un article et dans firebase
     deleteTitle (index) {
       try{
         BlogDb.child(index).remove()
@@ -136,6 +165,7 @@ export default {
       // .remove() //pour supprimer dans firebase
       // toastr.success('Article supprimé !')
     },
+    // modification d'un article et dans firebase
     editFile (index) {
       try{
         BlogDb.child(index).update({ edit: true })
@@ -143,6 +173,7 @@ export default {
         console.log('erreur edition :', e)
       }
     },
+    // Sauvegarde de la modification d'un article et dans firebase
     saveEdit (index) {
       const key = index['.key']
       try{
